b69a51f95945cf387614ee2eeee8a9113b98d10d
Corvus <erick.personal16@outlook.com>
2024-01-19 20:36:25 -0300

    awesome: main-panel: add clock widget

--- configs/awesome/layout/main-panel/init.lua
+++ configs/awesome/layout/main-panel/init.lua
@@ -3,6 +3,34 @@ local main_panel_screen = screen.primary
 local screen_height = main_panel_screen.geometry.height
 local dpi = Beautiful.xresources.apply_dpi
 
+local taglist = require("layout.main-panel.mods.taglist")(main_panel_screen)
+local tasklist = require("layout.main-panel.mods.tasklist")(main_panel_screen)
+local clock = Wibox.widget({
+  widget = Wibox.container.background,
+  bg = Beautiful.widget_bg_alt,
+  shape = Helpers.shape.rrect(Beautiful.small_radius),
+  {
+    widget = Wibox.container.margin,
+    bottom = dpi(5),
+    top = dpi(5),
+    {
+      layout = Wibox.layout.fixed.vertical,
+      {
+        widget = Wibox.widget.textclock,
+        format = "%H",
+        font = Beautiful.font_text .. "Medium 15",
+        halign = "center",
+      },
+      {
+        widget = Wibox.widget.textclock,
+        format = "%M",
+        font = Beautiful.font_text .. "Medium 15",
+        halign = "center",
+      },
+    },
+  },
+})
+
 local main_panel = Awful.wibar({
   screen = main_panel_screen,
   height = screen_height,
@@ -12,20 +40,13 @@ local main_panel = Awful.wibar({
   visible = true,
 })
 
-local taglist = require("layout.main-panel.mods.taglist")(main_panel_screen)
-local tasklist = require("layout.main-panel.mods.tasklist")(main_panel_screen)
-
 main_panel:setup({
-  layout = Wibox.layout.align.vertical,
+  widget = Wibox.container.margin,
+  margins = dpi(5),
   {
-    widget = Wibox.container.margin,
-    margins = dpi(5),
+    layout = Wibox.layout.align.vertical,
     taglist,
-  },
-  {
-    widget = Wibox.container.margin,
-    margins = dpi(5),
     tasklist,
+    clock,
   },
-  nil,
 })
