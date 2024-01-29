a5ae886b9e0413cbc9e28bf2aa2df2eea8886ccd
Corvus <erick.personal16@outlook.com>
2024-01-28 23:31:29 -0300

    awesome: add screenshots lib

---
+++ configs/awesome/utils/modules/screenshot.lua
@@ -0,0 +1,67 @@
+local PICTURES_FOLDER = Helpers.getCmdOut("xdg-user-dir PICTURES")
+local SC_FOLDER = "/Capturas/"
+
+local module = {}
+local defaults = {
+  prefix = "Captura_",
+  date_format = "%Y-%m-%d_%H:%M:%S",
+  file_format = "png",
+  hide_cursor = false,
+  quality = 8,
+  path = PICTURES_FOLDER .. SC_FOLDER,
+}
+
+local get_file_name = function(opts)
+  return opts.prefix .. os.date(opts.date_format) .. "." .. opts.file_format
+end
+
+local function main(self, opts)
+  opts = Gears.table.join(defaults, opts or {})
+  opts.delay = opts.delay or 0
+  local file_name = get_file_name(opts)
+  local cmd = self._private.cmd
+      .. "-f "
+      .. opts.file_format
+      .. " "
+      .. "-m "
+      .. tostring(opts.quality)
+      .. " "
+      .. (opts.hide_cursor and "-u" or "")
+      .. " "
+      .. opts.path
+      .. file_name
+  local timer = Gears.timer({
+    timeout = opts.delay,
+    autostart = true,
+    single_shot = true,
+    callback = function()
+      Awful.spawn.easy_async_with_shell(cmd, function()
+        self:emit_signal("file::saved", file_name, opts.path)
+      end)
+    end,
+  })
+end
+
+-- TODO: add def props in _private table
+local function new()
+  local ret = Gears.object({})
+  Gears.table.crush(ret, module, true)
+  ret._private = {}
+  ret._private.cmd = "maim -l -b 2 -c 0.35,0.55,0.85,0.25 "
+  return ret
+end
+
+function module.select(opts)
+  local self = new()
+  self._private.cmd = self._private.cmd .. "-s "
+  main(self, opts)
+  return self
+end
+
+function module.normal(opts)
+  local self = new()
+  main(self, opts)
+  return self
+end
+
+return module
