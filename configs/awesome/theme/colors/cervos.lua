1ee3594174af7234331150733520460d051e6a80
Corvus <erick.personal16@outlook.com>
2024-01-30 13:28:12 -0300

    awesome: add 2 color schemes

---
+++ configs/awesome/theme/colors/cervos.lua
@@ -0,0 +1,14 @@
+return {
+  foreground = "#c8c8db",
+  background = "#171819",
+  foreground_alt = "#171819",
+  black = "#2f3233",
+  red = "#dc7173",
+  green = "#78b892",
+  yellow = "#f4d392",
+  blue = "#759fe6",
+  magenta = "#ba93eb",
+  cyan = "#7cbdb3",
+  gray = "#a4a9b8",
+  orange = "#eba27f",
+}
---
+++ configs/awesome/theme/colors/everblush.lua
@@ -0,0 +1,14 @@
+return {
+  foreground = "#d1d1d1",
+  background = "#181f21",
+  foreground_alt = "#20292b",
+  black = "#20292b",
+    red           = "#e57474",
+    green         = "#8ccf7e",
+    yellow        = "#e5c76b",
+    blue          = "#67b0e8",
+    magenta       = "#c47fd5",
+    cyan          = "#7bc7c7",
+    gray          = "#c2c2c2",
+    orange        = "#e6a06e",
+}
