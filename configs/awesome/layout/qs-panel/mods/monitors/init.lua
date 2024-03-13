0dd000e0d00745695909dc438078f7bf056ae5b8
Corvus <erick.personal16@outlook.com>
2024-03-13 20:52:45 -0300

    awesome: qs-panel: monitors: implemented update function

--- configs/awesome/layout/qs-panel/mods/monitors/init.lua
+++ configs/awesome/layout/qs-panel/mods/monitors/init.lua
@@ -1,21 +1,45 @@
 local dpi = Beautiful.xresources.apply_dpi
 local mkmonitor = require("layout.qs-panel.mods.monitors.base")
+local function getPercentage(value, total)
+  return (value / total) * 100 + 0.5
+end
 
 local disk_monitor = mkmonitor({
   icon = "󰆓",
   color = Beautiful.red,
+  update_fn = function(self)
+    awesome.connect_signal("lib:disk", function(disk)
+      local partition = disk["/home"]
+      self:set_value(partition.perc)
+    end)
+  end,
 })
 local cpu_monitor = mkmonitor({
   icon = "󰍛",
   color = Beautiful.blue,
+  update_fn = function(self)
+    awesome.connect_signal("lib::cpu", function(cpu_usage)
+      self:set_value(cpu_usage)
+    end)
+  end,
 })
 local ram_monitor = mkmonitor({
   icon = "󰆼",
   color = Beautiful.magenta,
+  update_fn = function(self)
+    awesome.connect_signal("lib::ram", function(ram)
+      self:set_value(getPercentage(ram.used, ram.total))
+    end)
+  end,
 })
 local temp_monitor = mkmonitor({
   icon = "󰔏",
   color = Beautiful.orange or Beautiful.yellow,
+  update_fn = function(self)
+    awesome.connect_signal("lib::temperature", function(temp)
+      self:set_value(temp)
+    end)
+  end,
 })
 
 return Wibox.widget({
