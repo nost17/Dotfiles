0cd0f3271ccd21f411ae48e6b385a8106cb61e4a
Rreverie <erick.personal16@outlook.com>
2024-06-03 15:51:35 -0300

    Moved titlebar files

--- ui/init.lua
+++ ui/init.lua
@@ -3,6 +3,9 @@
 local awful = require("awful")
 local main_panel = require("ui.panels.main-panel")
 
+require(... .. ".titlebar")
+require(... .. ".notification")
+
 screen.connect_signal("request::desktop_decoration", function(s)
   -- Create all tags and attach the layouts to each of them.
   awful.tag(User.config.tags, s, awful.layout.layouts[1])
--- ui/titlebar/init.lua
+++ ui/titlebar/init.lua
@@ -1,5 +1,13 @@
 -- Returns titlebars for normal clients, this structure allows one to 
 -- easily define special titlebars for particular clients.
-return {
-   normal = require(... .. '.normal')
-}
+-- Add a titlebar if titlebars_enabled is set to true for the client in `config/rules.lua`.
+
+local base = require(... .. ".base")
+
+client.connect_signal('request::titlebars', function(c)
+   -- While this isn't actually in the example configuration, it's the most sane thing to do.
+   -- If a client expressly says not to draw titlebars on it, just don't.
+   if c.requests_no_titlebars then return end
+
+   base(c)
+end)
