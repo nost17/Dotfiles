109c205aa433cd1cf2e9546bae2eee8f19a48351
Rreverie <erick.personal16@outlook.com>
2024-06-03 13:43:35 -0300

    Move wibar -> main-panel

--- signal/screen.lua
+++ signal/screen.lua
@@ -2,15 +2,8 @@ local awful     = require('awful')
 local beautiful = require('beautiful')
 local wibox     = require('wibox')
 
-local widgets   = require('ui')
-
 --- Attach tags and widgets to all screens.
-screen.connect_signal('request::desktop_decoration', function(s)
-   -- Create all tags and attach the layouts to each of them.
-   awful.tag(require('config.user').tags, s, awful.layout.layouts[1])
-   -- Attach a wibar to each screen.
-   widgets.wibar(s)
-end)
+
 
 --- Wallpaper.
 -- NOTE: `awful.wallpaper` is ideal for creating a wallpaper IF YOU
--- ui/init.lua
+++ ui/init.lua
@@ -1,7 +1,11 @@
 -- Returns all widgets, with assigned names, in a table.
-return {
-   menu         = require(... .. '.menu'),
-   notification = require(... .. '.notification'),
-   titlebar     = require(... .. '.titlebar'),
-   wibar        = require(... .. '.wibar')
-}
+
+local awful = require("awful")
+local main_panel = require("ui.panels.main-panel")
+
+screen.connect_signal("request::desktop_decoration", function(s)
+  -- Create all tags and attach the layouts to each of them.
+  awful.tag(require("config.user").tags, s, awful.layout.layouts[1])
+  -- Attach a wibar to each screen.
+  main_panel(s)
+end)
